8c19276f7b5fb1528a59c98e0c0196ef3e74b112
Jagdish9495 <4jagdishb@gmail.com>
2022-07-27 00:04:29 +0530

    2nd commit

---
+++ src/pixon/Pixel123.java
@@ -0,0 +1,12 @@
+package pixon;
+
+public class Pixel123 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("5 is odd number");
+		
+		System.out.println("6 is even number");
+	}
+
+}
