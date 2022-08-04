fe0f9542c8235963edeb2c3cbb77d983496b0fb3
Jagdish9495 <110035511+Jagdish9495@users.noreply.github.com>
2022-08-04 10:08:42 +0530

    Update Pixel.java

--- src/pixon/Pixel.java
+++ src/pixon/Pixel.java
@@ -5,6 +5,8 @@ public class Pixel
 	public static void main(String[] args) 
 	{
 		System.out.println("hello let's do addition");
+		System.out.println("hello let's do");
+		System.out.println("hello");
 	}
 
 }
